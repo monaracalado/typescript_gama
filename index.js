7f999ef9119fc403f15933928ccecd5c71911dd6
Monara Calado <monaracalado@gmail.com>
2021-08-11 23:49:59 -0300

    add index

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+var meuNumero1 = 10;
+var meuNumero2 = 5;
+var resultado = soma(meuNumero1, meuNumero2);
+console.log(resultado);
