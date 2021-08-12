e74d0cdff0659ef7894370d4b41106c736309892
Monara Calado <monaracalado@gmail.com>
2021-08-12 04:23:55 -0300

    add index

---
+++ index.ts
@@ -0,0 +1,10 @@
+function soma(numero1: number, numero2: number) {
+    return numero1 + numero2;
+  }
+  
+  const meuNumero1 = 10;
+  const meuNumero2 = 5;
+  
+  const resultado = soma(meuNumero1, meuNumero2);
+  console.log(resultado);
+  
